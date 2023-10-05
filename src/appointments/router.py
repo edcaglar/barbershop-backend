b38ce48d61b536e2beea15d9764eb71ecdff7e9c
edcaglar <edcaglar@gmail.com>
2023-10-05 20:37:39 +0300

    temp

--- src/appointments/router.py
+++ src/appointments/router.py
@@ -3,8 +3,10 @@ from fastapi import status, HTTPException
 from sqlalchemy.orm import Session
 from typing import Annotated
 from datetime import date, time
+
 from src.database import get_db
 from . import models, schemas, service
+from src.barbers import service as barber_service
 
 router = APIRouter()
 
@@ -27,12 +29,17 @@ def get_appointments(skip: int = 0, limit: int = 100, db: Session = Depends(get_
 
 @router.get("/{barber_id}", status_code=status.HTTP_200_OK, response_model=list[schemas.Appointment])
 def get_appointments_by_barber_id(barber_id: int, db: Session = Depends(get_db)):
-    return service.get_by_barber_id(db=db, barber_id=barber_id)
+    appointments = service.get_by_barber_id(db=db, barber_id=barber_id)
+    if not appointments:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="There is no appointments")
+    return appointments
 
 @router.get("/{barber_id}/{appointment_date}", status_code=status.HTTP_200_OK, response_model=list[schemas.Appointment])
 def get_appointments_by_barber_id_on_date(barber_id: int, appointment_date: date, db: Session = Depends(get_db)):
-
-    return service.get_by_barber_id_on_date(db=db, barber_id=barber_id, appointment_date=appointment_date)
+    appointments = service.get_by_barber_id_on_date(db=db, barber_id=barber_id, appointment_date=appointment_date)
+    if not appointments:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="There is no appointments")
+    return appointments
 
 @router.get("/{barber_id}/{appointment_date}/{appointment_time}", status_code=status.HTTP_200_OK, response_model=schemas.Appointment)
 def get_appointment_by_barber_id_on_datetime(barber_id: int, appointment_date: date, appointment_time: str, db: Session = Depends(get_db)):
--- src/appointments/service.py
+++ src/appointments/service.py
@@ -3,7 +3,6 @@ from datetime import datetime, date, time
 from fastapi import HTTPException
 
 from . import models, schemas
-from src.barbers import service as barber_service
 
 def get(db:Session, id: int):
     return db.query(models.Appointment).filter(models.Appointment.id == id).first()
@@ -13,9 +12,6 @@ def get_all(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Appointment).offset(skip).limit(limit).all()
 
 def create(db: Session, appointment: schemas.AppointmentCreate):
-    barber = barber_service.get(db=db, id=appointment.barber_id)
-    if not barber:
-        raise HTTPException(status_code=404, detail="Barber not found.")
     new_appointment = models.Appointment(**appointment.model_dump())
     db.add(new_appointment)
     db.commit()
@@ -24,32 +20,23 @@ def create(db: Session, appointment: schemas.AppointmentCreate):
 
 
 def get_by_credentials(db: Session, customer_surname: str, customer_phone: str):
-    appointments = db.query(models.Appointment).filter(models.Appointment.customer_surname == customer_surname,
+    return db.query(models.Appointment).filter(models.Appointment.customer_surname == customer_surname,
                                                        models.Appointment.customer_phone == customer_phone).all()
-    if not appointments:
-        raise HTTPException(status_code=404, detail="Appointment not found.")
-    return appointments
+
 
 def get_by_barber_id(db: Session, barber_id: int):
-    appointments = db.query(models.Appointment).filter(models.Appointment.barber_id == barber_id).all()
-    if not appointments:
-        raise HTTPException(status_code=404, detail="There is no appointments.")
-    return appointments
+    return db.query(models.Appointment).filter(models.Appointment.barber_id == barber_id).all()
+
 
 def get_by_barber_id_on_date(db: Session, barber_id: int, appointment_date: date):
-    appointments = db.query(models.Appointment).filter(models.Appointment.barber_id == barber_id,
+    return db.query(models.Appointment).filter(models.Appointment.barber_id == barber_id,
                                                models.Appointment.appointment_date == appointment_date).all()
-    if not appointments:
-        raise HTTPException(status_code=404, detail="There is no appointments.")
-    return appointments
 
 def get_by_barber_id_on_datetime(db: Session, barber_id: int, appointment_date: date, appointment_time: str):
-    appointments = db.query(models.Appointment).filter(models.Appointment.barber_id == barber_id,
+    return db.query(models.Appointment).filter(models.Appointment.barber_id == barber_id,
                                                models.Appointment.appointment_date == appointment_date,
                                                models.Appointment.appointment_time == appointment_time).first()
-    if not appointments:
-        raise HTTPException(status_code=404, detail="There is no appointments.")
-    return appointments
+
 
 
 
